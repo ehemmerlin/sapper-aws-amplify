8b8e274dfb89a9ac6de65dd304d30061f1d606af
Eric Hemmerlin <ehemmerlin@users.noreply.github.com>
2019-10-14 20:37:38 +0200

    Initial commit

---
+++ src/client.js
@@ -0,0 +1,31 @@
+import * as sapper from '@sapper/app';
+import Amplify from 'aws-amplify';
+import config from './config/aws';
+
+Amplify.configure({
+	Auth: {
+		mandatorySignIn: true,
+		region: config.cognito.REGION,
+		userPoolId: config.cognito.USER_POOL_ID,
+		identityPoolId: config.cognito.IDENTITY_POOL_ID,
+		userPoolWebClientId: config.cognito.APP_CLIENT_ID
+	},
+	Storage: {
+		region: config.s3.REGION,
+		bucket: config.s3.BUCKET,
+		identityPoolId: config.cognito.IDENTITY_POOL_ID
+	},
+	API: {
+		endpoints: [
+			{
+				name: "notes",
+				endpoint: config.apiGateway.URL,
+				region: config.apiGateway.REGION
+			},
+		]
+	}
+});
+
+sapper.start({
+	target: document.querySelector('#sapper')
+});
